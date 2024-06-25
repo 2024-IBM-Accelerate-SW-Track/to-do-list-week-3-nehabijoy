018b3144875f789bf53afc76e4f3ac77e4b69e95
Neha Bijoy <nehabijoy@vt.edu>
2024-06-24 21:44:19 -0400

    initial commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,38 @@
-import React, { Component } from "react";
-import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import React, { Component } from 'react';
 
+import "./About.css";
+
+import profileImage from "../assets/profile_pic.jpg";
+
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img 
               className="profile_image"
-              src={profile_pic}
+            
+              src = {profileImage}
               alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              ></img>
+            </div>
+            </div>
+             <div className="split right"> 
+              <div className="centered">
+              <div className="name_title">Neha Bijoy</div>
+              <div className="brief_description">
+              Hi! I'm Neha and I'm a rising Junior at Virginia Tech. I'm majoring in Computer Science and minoring in Human Computer Interaction. 
+              I'm super excited to be a part of IBM's Accelerate, I can't wait to meet new friends and grow both professionally and technically.
+              </div>
             </div>
           </div>
         </div>
+
+
       </div>
-    );
+    )
   }
 }
